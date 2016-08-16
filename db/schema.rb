7acb0fc6e1742d77755549be237210c8e83529e9
Dave Charnuska <davecharnuska@gmail.com>
2016-08-16 11:18:49 -0400

    add questions model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815211251) do
+ActiveRecord::Schema.define(version: 20160816151624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20160815211251) do
 
   add_index "polls", ["author_id"], name: "index_polls_on_author_id", using: :btree
   add_index "polls", ["title"], name: "index_polls_on_title", using: :btree
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "text",       null: false
+    t.integer  "poll_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "questions", ["poll_id"], name: "index_questions_on_poll_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "user_name",  null: false
