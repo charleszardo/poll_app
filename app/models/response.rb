f6d34e0ca3696e3dd5994eed6b9eef991445766d
Dave Charnuska <davecharnuska@gmail.com>
2016-08-16 13:59:10 -0400

    add #sibling_responses, clean up associations

--- app/models/response.rb
+++ app/models/response.rb
@@ -10,9 +10,15 @@
 #
 
 class Response < ActiveRecord::Base
-  validates :user_id, :answer_id, :presence => true
+  validates :user_id, :answer_id, presence: true
 
-  belongs_to :answer_choice, class_name: "AnswerChoice", foreign_key: :answer_id, primary_key: :id
+  belongs_to :answer_choice, class_name: "AnswerChoice", foreign_key: :answer_id
 
-  belongs_to :respondent, class_name: "User", foreign_key: :user_id, primary_key: :id
+  belongs_to :respondent, class_name: "User"
+
+  has_one :question, through: :answer_choice
+
+  def sibling_responses
+    self.question.responses.where.not(id: self.id)
+  end
 end
