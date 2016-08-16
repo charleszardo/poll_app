99d40b7202f62e6c722a5dbc4842ebe0511055b9
Dave Charnuska <davecharnuska@gmail.com>
2016-08-16 12:01:26 -0400

    add Response validations and associations

--- app/models/response.rb
+++ app/models/response.rb
@@ -10,4 +10,9 @@
 #
 
 class Response < ActiveRecord::Base
+  validates :user_id, :answer_id, :presence => true
+
+  belongs_to :answer_choice
+
+  belongs_to :respondent, class_name: "User", foreign_key: :user_id, primary_key: :id
 end
