69a0b45f3eeb08dbb044f2295a614a62e292aaee
Dave Charnuska <davecharnuska@gmail.com>
2016-08-16 11:58:48 -0400

    add poll and user associations

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -16,4 +16,8 @@
 
 class Poll < ActiveRecord::Base
   validates :title, :author_id, :presence => true
+
+  belongs_to :author, class_name: "User", foreign_key: :author_id, primary_key: :id
+
+  has_many :questions
 end
