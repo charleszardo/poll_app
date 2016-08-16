e4484a784923c58e05467c897add24c130f2c005
Dave Charnuska <davecharnuska@gmail.com>
2016-08-16 11:31:48 -0400

    add User model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  user_name  :string           not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  validates :user_name, :presence => true, :uniqueness => true
+end
