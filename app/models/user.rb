927e4c77a9393e56040f321b484467a4c622d756
Dave Charnuska <davecharnuska@gmail.com>
2016-08-17 15:41:07 -0400

    begin User#completed_polls

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,22 @@ class User < ActiveRecord::Base
   has_many :authored_polls, class_name: "Poll", foreign_key: :author_id, primary_key: :id
 
   has_many :responses
+
+  def completed_polls
+    polls = Poll.find_by_sql([<<-SQL, id])
+      SELECT
+        polls.title, COUNT(questions.id) as q_count
+      FROM
+        polls
+      JOIN
+        questions ON questions.poll_id = polls.id
+      GROUP BY
+        polls.id
+    SQL
+
+    polls.each do |poll|
+      p poll.title
+      p poll.q_count
+    end
+  end
 end
