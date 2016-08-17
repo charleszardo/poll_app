d5404d585f35125f81ce1e79a18166d915715bae
Dave Charnuska <davecharnuska@gmail.com>
2016-08-17 15:21:20 -0400

    refactor Question#results

--- app/models/question.rb
+++ app/models/question.rb
@@ -23,10 +23,22 @@ class Question < ActiveRecord::Base
   has_many :responses, through: :answer_choices, source: :responses
 
   def results
-    results = {}
-    answer_choices.includes(:responses).each do |ac|
-      results[ac.text] = ac.responses.length
+    acs = AnswerChoice.find_by_sql([<<-SQL, id])
+      SELECT
+        answer_choices.text AS text, COUNT(responses.id) AS num_responses
+      FROM
+        answer_choices
+      LEFT OUTER JOIN
+        responses ON responses.answer_id = answer_choices.id
+      WHERE
+        answer_choices.question_id = ?
+      GROUP BY
+        answer_choices.id
+      SQL
+
+    acs.inject({}) do |results, ac|
+      results[ac.text] = ac.num_responses;
+      results
     end
-    results
   end
 end
